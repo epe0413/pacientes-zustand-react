7a3871b229217ae76d5dedbd1f843ea48dd20485
Everth Paredes <epe0413@gmail.com>
2024-05-06 20:40:08 -0600

    Introduction to React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ function App() {
 
   return (
     <>
-      <div className="container mx-auto mt-5">
+      <header className="bg-white py-2 shadow-md w-full md:fixed md:top-0 md:left-0">
         <h1 className="font-black text-2xl text-center md:w-2/3 md:mx-auto">
           Seguimiento de Pacientes {''}
           <span className="text-indigo-700 ">Veterinaria</span>
         </h1>
+      </header>
 
-        <div className="mt-8 md:flex">
+      <div className="container mx-auto mt-5 md:pt-6">
+
+        <div className="mt-4 md:flex">
           <PatientForm/>
           <PatientsList/>
         </div>
